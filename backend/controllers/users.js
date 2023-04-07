21ed9cdd1fcc7f874bdb4a043246c7c526b07c7c
Pavel Bondarevskii <gohre94@gmail.com>
2023-04-07 08:07:01 +0300

    after review

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -92,7 +92,7 @@ module.exports.login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'development' ? JWT_SECRET : 'secret-key-dev', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'secret-key-dev', { expiresIn: '7d' });
       res.send({ token });
     })
     .catch(next);
